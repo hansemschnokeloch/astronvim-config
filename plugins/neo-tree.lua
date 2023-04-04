f67de3e421b23b0441a6fbac57afab621c50446c
pascal <sarl@hubrecht.info>
2023-04-04 17:53:25 +0200

    add neo-tree

---
+++ plugins/neo-tree.lua
@@ -0,0 +1,23 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = function(_, opts)
+    opts.window.width = 40
+    opts.default_component_configs.indent = { indent_size = 2, padding = 1 }
+    opts.event_handlers = {
+      {
+        event = "file_opened",
+        handler = function(file_path)
+          --auto close
+          require("neo-tree").close_all()
+        end,
+      },
+    }
+    opts.filesystem = {
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = true,
+        hide_gitignored = true,
+      },
+    }
+  end,
+}
