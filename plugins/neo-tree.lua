b269d985b94cd60740e7d841da4297ee7ebaa5f4
pascal <sarl@hubrecht.info>
2023-04-04 20:39:28 +0200

    neo tree fix

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,9 +1,7 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = function(_, opts)
-    opts.window = {
-      width = 40,
-    }
+    opts.window.width = 40
     opts.default_component_configs.indent = { indent_size = 2, padding = 1 }
     opts.event_handlers = {
       {
@@ -13,16 +11,6 @@ return {
           require("neo-tree").close_all()
         end,
       },
-    }
-    -- opts.filesystem = {
-    --   filtered_items = {
-    --     visible = true, -- If you set this to `true`, all "hide" just mean "dimmed out"
-    --     -- hide_dotfiles = true,
-    --     -- hide_gitignored = true,
-    --   },
-    -- }
-    opts.sources = {
-      "filesystem",
     }
   end,
 }
