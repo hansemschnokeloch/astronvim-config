015aa49cf018948f604debbdfc41fe78aaf6daec
pascal <sarl@hubrecht.info>
2023-04-04 17:57:27 +0200

    update comment

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -14,7 +14,7 @@ return {
     }
     opts.filesystem = {
       filtered_items = {
-        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        visible = true, -- If you set this to `true`, all "hide" just mean "dimmed out"
         hide_dotfiles = true,
         hide_gitignored = true,
       },
