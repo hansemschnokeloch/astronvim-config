2dbbf2bc17729ffa6ac9b1d0ef981405600d3a4d
pascal <sarl@hubrecht.info>
2023-04-12 17:51:25 +0200

    disable space mapping

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -4,6 +4,7 @@ return {
     opts.window.width = 60
     opts.window.mappings = {
       ["s"] = "prev_source",
+      ["<space>"] = false, -- disable space until we figure out which-key disabling
     }
     opts.default_component_configs.indent = { indent_size = 2, padding = 1 }
     opts.event_handlers = {
